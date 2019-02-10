1300477d6378ab20766141cb381f8c274b761951
Sarah Hailey <sarah.elizabeth.hailey@gmail.com>
2019-02-10 04:18:13 -0800

    CONFLICTZ

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const ascii = require('./ascii')
 const port = process.env.PORT || 1337
 
 app.use(morgan('tiny'))
-
+YOU HAVE NO CHANCE TO SURVIVE MAKE YOUR TIME
+HA HA HA 
 // body parsing middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -17,6 +18,19 @@ if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
 }
+
+
+// app.get('/articles', async (req, res, next) => {
+//     try {
+// 	const articles = await Article.findAll()
+// 	console.log(articles)
+
+// 	res.status(201).send(articles)
+//     } catch (err) {
+// 	next(err)
+//     }
+// })
+
 app.get('*', (req, res) =>
   res.sendFile(path.resolve(__dirname, '..', 'client', 'public', 'index.html'))
 )
