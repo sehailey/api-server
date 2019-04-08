f49c0f1edf0971dd8251d14409230841061617c5
Sarah Hailey <sarah.elizabeth.hailey@gmail.com>
2019-04-07 22:25:09 -0400

    updated item api routes

--- api/items.js
+++ api/items.js
@@ -6,7 +6,7 @@ router.get('/', async (req, res, next) => {
   try {
     const items = await Item.findAll()
 
-    res.status(201).send(items)
+    res.send(items)
   } catch (err) {
     next(err)
   }
@@ -14,7 +14,7 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
   try {
-    const item = await Item.findById(req.params.id)
+    const item = await Item.findByPk(req.params.id)
     res.json(item)
   } catch (err) {
     next(err)
@@ -25,7 +25,27 @@ router.post('/', async (req, res, next) => {
   try {
     const item = await Item.create(req.body)
 
-    res.status(201).json(item)
+    res.json(item)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const item = await Item.create(req.body)
+
+    res.json(item)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const item = await Item.findByPk(req.params.id)
+    await item.update({name: req.body.name})
+    res.json(item)
   } catch (err) {
     next(err)
   }
